2043e30ee2b00575afafda0f6e4c4afeaddb63be
Matt Norby <31460995+mattnorby@users.noreply.github.com>
2018-05-13 22:11:38 -0400

    Water adjustments, option to hardcode kayak pts
    1. Adjust water in the kayak (source) image to match water in the Koln (destination) image.
    2. Provide a compile switch that bypasses manual selection of the points in the source image, and uses hardcoded values instead.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ using namespace std;
 vector<Point2i> pts;
 
 #define RESIZE_FACTOR 8
+//#define USE_HARDCODED_POINTS 1
 
 void handleMouseEvent(int event, int x, int y, int flags, void* userdata) {
     // When the user left-clicks...
@@ -43,24 +44,56 @@ int main(int argc, char *argv[]) {
     }
 
     // Resize the image, so that the kayak is a believable size.
+    cout << "Resize the source image" << endl;
     resize(img, img, Size2i(img.cols / RESIZE_FACTOR, img.rows / RESIZE_FACTOR));
 
     // The photo of the kayak is in better focus than the target image.
     // Blur the kayak to compensate.
-    blur(img, img, Size(3, 3));
+    cout << "Blur the source image" << endl;
+    Mat blurImg = img.clone();
+    blur(img, blurImg, Size(3, 3));
 
+    // Seamless cloning will attempt to blend colors around the edges,
+    // but not so much in the middle.  Because the water colors are a bit
+    // different, we need to darken the water in the kayak photo.  To do that,
+    // we use a lookup table.  OpenCV applies it to each channel (RGB).
+    // The approach used here is based on observations of pixel values from
+    // the kayak water (RGB 100..180) and the Koln water (RGB 100..140).
+    cout << "Apply color lookup table to source image" << endl;
+    Mat lookupTable(1, 256, CV_8U);
+    uchar* ltPtr = lookupTable.ptr();
+    for (int i = 0; i < 256; i++) {
+        if (i <= 100 || i > 180) {
+            // Values outside our range of interest are unchanged
+            ltPtr[i] = i;
+        } else {
+            // Ex. 140 --> 120, 160 --> 130, 180 --> 140
+            ltPtr[i] = ((i - 100) / 2) + 100;
+        }
+    }
+    LUT(blurImg, lookupTable, img);
+
+    cout << "Obtain source image mask" << endl;
+#ifdef USE_HARDCODED_POINTS
+    // These points were used to generate the clone.jpg in the repo.
+    pts.push_back(Point2i(383, 196));
+    pts.push_back(Point2i(239, 199));
+    pts.push_back(Point2i(136, 211));
+    pts.push_back(Point2i( 26, 103));
+    pts.push_back(Point2i( 71,   1));
+    pts.push_back(Point2i(124,   1));
+    pts.push_back(Point2i(383, 104));
+#else
     // Show the kayak, and let the user click points in the image.
     // When the user presses any key on the keyboard, cloning proceeds.
     // For best results, the user should loosely lasso the kayak,
     // paddlers, and the reflection in the water.
-    // Here are the points that were used to create the clone.jpg in the repo:
-    // (383, 196), (239, 199), (136, 211), (26, 103), (71, 1),
-    // (124, 1), (383, 104)
     namedWindow("Kayak", CV_WINDOW_AUTOSIZE);
     setMouseCallback("Kayak", handleMouseEvent, 0);
     imshow("Kayak", img);
     waitKey(0);
     setMouseCallback("Kayak", 0, 0);
+#endif // USE_HARDCODED_POINTS
 
     // We need at least three points for a polygonal region.
     if (pts.size() >= 3) {
@@ -72,13 +105,14 @@ int main(int argc, char *argv[]) {
         drawContours(mouseMask, mouseContours, 0, Scalar(255, 255, 255), CV_FILLED);
 
         // Show the user the region of the kayak that was actually selected.
-        // This step is just for visualization, not necessary for cloning.
+        cout << "Show source image mask" << endl;
         Mat selectedRegion(img.size(), img.type(), Scalar(0, 0, 0));
         bitwise_and(img, mouseMask, selectedRegion);
         namedWindow("Selected Region", CV_WINDOW_AUTOSIZE);
         imshow("Selected Region", selectedRegion);
 
         // Load the target image
+        cout << "Load target image" << endl;
         Mat imgTarget = imread("koln1997.jpg", IMREAD_COLOR);
         if (imgTarget.empty()) {
             cout << "Cannot open image: koln1997.jpg" << endl;
@@ -93,6 +127,7 @@ int main(int argc, char *argv[]) {
         // We will restore this area after cloning.
         // Because the tolerances are lower for these selections, the
         // pixels were selected via mouse, and hardcoded here.
+        cout << "Create a mask for the foreground of the target image" << endl;
         vector<vector<Point2i> > fgContours;
         vector<Point2i> fgPts;
         fgPts.push_back(Point2i(860, 1184));
@@ -115,7 +150,7 @@ int main(int argc, char *argv[]) {
         seamlessClone(img, imgTarget, mouseMask, centerPt, imgFinal, NORMAL_CLONE);
 
         // Restore the foreground portion, using the mask we created earlier.
-        cout << "Restore foreground portion" << endl;
+        cout << "Restore foreground portion of target image" << endl;
         imgTarget.copyTo(imgFinal, fgMask);
 
         cout << "Write output image to file" << endl;
